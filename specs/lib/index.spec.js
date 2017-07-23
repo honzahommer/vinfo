6a33d107d542c488782fad76a984ff0b98f03b90
Honza Hommer <honza@hommer.cz>
2017-07-23 12:08:13 +0200

    Base VIN validation and decode

--- specs/lib/index.spec.js
+++ specs/lib/index.spec.js
@@ -1,13 +1,40 @@
 import { expect } from 'chai';
 import vinfo from '../../src/lib/index';
 
+const vin = vinfo('WVWUK63B92P546818');
+const res = vin.decode().result;
+const eql = {
+  continent: 'Europe',
+  country: 'West Germany',
+  make: 'Volkswagen',
+  year: 2002,
+};
+
 /** @test {vinfo} */
 describe('vinfo', () => {
-  it('exists', () => {
-    expect(vinfo).to.exist;
+  it('returns {}', () => {
+    expect(vin).to.be.object;
   });
 
-  it('returns true', () => {
-    expect(vinfo()).to.be.true;
+  describe('validate', () => {
+    it('returns true', () => {
+      expect(vin.validate()).to.be.true;
+    });
+
+    it('returns false', () => {
+      expect(vin.validate('WVWUK63B82P546818')).to.be.false;
+    });
+  });
+
+  describe('decode', () => {
+    it('returns {}', () => {
+      expect(res).to.be.object;
+    });
+
+    Object.keys(eql).forEach((key) => {
+      it(`${key} is ${eql[key]}`, () => {
+        expect(res[key]).to.be.equal(eql[key]);
+      });
+    });
   });
 });
